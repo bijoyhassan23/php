039e04a3bfffcb2532ce2d16ac5aac3fad457f6e
Saif Uddin Biijoy <Bijoyhassan23@gmail.com>
2024-08-15 08:03:51 +0600

    new m

--- php_send_mail.php
+++ php_send_mail.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 /**
  * mail(to, subject, message, headers)
@@ -12,3 +20,5 @@
 mail($to, $subject, $message, $header);
 echo "mail send";
 ?>
+</body>
+</html>
